1882f48d9819a0851d7a750022417b5e972e98a4
Marek Wasiewicz <marekwasiewicz@MacBook-Air-Marek.local>
2024-12-04 00:28:20 +0100

    First commit on branch Marek

--- app/models.py
+++ app/models.py
@@ -29,7 +29,7 @@ class Team(db.Model):
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(50),nullable=False)
+    firstName = db.Column(db.String(51),nullable=False)
     lastName = db.Column(db.String(50),nullable=False)
     age = db.Column(db.Integer,nullable=False)
     position = db.Column(db.Enum('substitute','field',name='player_position_enum'))
